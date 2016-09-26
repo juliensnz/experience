356bebd330f7e831ecc6a0e50ad7082e3b18994e
Julien Sanchez <sumbobyboys@gmail.com>
2016-09-26 17:15:40 +0200

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  devtool: 'source-map',
+  entry: [
+    'webpack-dev-server/client?http://localhost:3000',
+    'webpack/hot/only-dev-server',
+    './src/index.tsx'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/static/'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  resolve: {
+    extensions: ["", ".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
+  },
+  module: {
+    loaders: [{
+      test: /\.tsx?$/,
+      loaders: ['react-hot', 'ts-loader'],
+      include: path.join(__dirname, 'src')
+    }],
+    preLoaders: [
+      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+      { test: /\.js$/, loader: "source-map-loader" }
+    ]
+  }
+};
